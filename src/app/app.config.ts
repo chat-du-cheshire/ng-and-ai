115ac66661979bf66af133283cb700e9e12c3f82
Max Ivanov <norrett@gmail.com>
2025-11-14 08:30:15 +0300

    add tool

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,22 @@
 import {
-    ApplicationConfig,
-    provideBrowserGlobalErrorListeners,
-    provideZonelessChangeDetection,
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHashbrown } from '@hashbrownai/angular';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideBrowserGlobalErrorListeners(),
-        provideZonelessChangeDetection(),
-        provideRouter(routes),
-        provideHashbrown({
-            baseUrl: 'http://localhost:3000/chat',
-        }),
-    ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideHashbrown({
+      baseUrl: 'http://localhost:3000/chat',
+    }),
+  ],
 };
--- src/app/features/chat/chat.ts
+++ src/app/features/chat/chat.ts
@@ -1,95 +1,114 @@
-import { ChangeDetectionStrategy, Component, effect } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+} from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatFabButton } from '@angular/material/button';
-import { chatResource } from '@hashbrownai/angular';
+import { chatResource, createTool } from '@hashbrownai/angular';
 import { MatCard, MatCardContent } from '@angular/material/card';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
+import { ShowsLoader } from '../../core/shows-loader';
 
 @Component({
-    selector: 'app-chat',
-    imports: [
-        MatFormField,
-        MatLabel,
-        MatInput,
-        MatFabButton,
-        MatIcon,
-        MatLabel,
-        MatCard,
-        MatCardContent,
-    ],
-    template: `
-        <div class="chat-container">
-            <div class="chat-input">
-                <mat-form-field>
-                    <mat-label>How can I help you?</mat-label>
-                    <input
-                        #inputField
-                        matInput
-                        (keydown.enter)="sendMessage(inputField.value)"
-                    />
-                </mat-form-field>
+  selector: 'app-chat',
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatFabButton,
+    MatIcon,
+    MatLabel,
+    MatCard,
+    MatCardContent,
+  ],
+  template: `
+    <div class="chat-container">
+      <div class="chat-input">
+        <mat-form-field>
+          <mat-label>How can I help you?</mat-label>
+          <input
+            #inputField
+            matInput
+            (keydown.enter)="sendMessage(inputField.value)"
+          />
+        </mat-form-field>
 
-                <button
-                    matFab
-                    color="primary"
-                    class="send-button"
-                    aria-label="Send message"
-                    (click)="sendMessage(inputField.value)"
-                >
-                    <mat-icon>send</mat-icon>
-                </button>
-            </div>
+        <button
+          matFab
+          color="primary"
+          class="send-button"
+          aria-label="Send message"
+          (click)="sendMessage(inputField.value)"
+        >
+          <mat-icon>send</mat-icon>
+        </button>
+      </div>
 
-            <div class="chat-messages">
-                @for (message of chat.value(); track $index) {
-                    @switch (message.role) {
-                        @case ('user') {
-                            <mat-card class="message user">
-                                <mat-card-content>
-                                    <p>{{ message.content }}</p>
-                                </mat-card-content>
-                            </mat-card>
-                        }
-                        @case ('assistant') {
-                            <div class="assistant-message-container">
-                                <div class="assistant-avatar">
-                                    <mat-icon
-                                        aria-hidden="false"
-                                        aria-label="Assistant avatar"
-                                        fontIcon="face_2"
-                                    ></mat-icon>
-                                </div>
-                                <mat-card class="message assistant">
-                                    <mat-card-content>
-                                        <p>{{ message.content }}</p>
-                                    </mat-card-content>
-                                </mat-card>
-                            </div>
-                        }
-                    }
-                }
-            </div>
-        </div>
-    `,
-    styleUrl: './chat.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush,
+      <div class="chat-messages">
+        @for (message of chat.value(); track $index) {
+          @switch (message.role) {
+            @case ('user') {
+              <mat-card class="message user">
+                <mat-card-content>
+                  <p>{{ message.content }}</p>
+                </mat-card-content>
+              </mat-card>
+            }
+            @case ('assistant') {
+              <div class="assistant-message-container">
+                <div class="assistant-avatar">
+                  <mat-icon
+                    aria-hidden="false"
+                    aria-label="Assistant avatar"
+                    fontIcon="face_2"
+                  ></mat-icon>
+                </div>
+                <mat-card class="message assistant">
+                  <mat-card-content>
+                    <p>{{ message.content }}</p>
+                  </mat-card-content>
+                </mat-card>
+              </div>
+            }
+          }
+        }
+      </div>
+    </div>
+  `,
+  styleUrl: './chat.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Chat {
-    chat = chatResource({
-        model: 'gpt-4o',
-        system: `You are a friendly sarcastic chat bot`,
-    });
+  #showsLoader = inject(ShowsLoader);
 
-    #effectRef = effect(() => {
-        console.log(this.chat.value());
-    });
+  chat = chatResource({
+    model: 'gpt-4o',
+    system: `You are a friendly sarcastic chat bot`,
+    tools: [
+      createTool({
+        name: 'getShows',
+        description:
+          'A tool to list available TV Shows. Call this tool whenever the user asks for shows',
+        handler: () => {
+          return lastValueFrom(this.#showsLoader.loadAll());
+        },
+      }),
+    ],
+  });
 
-    sendMessage(message: string) {
-        this.chat.sendMessage({
-            role: 'user',
-            content: message.trim(),
-        });
-    }
+  #effectRef = effect(() => {
+    console.log(this.chat.value());
+  });
+
+  sendMessage(message: string) {
+    this.chat.sendMessage({
+      role: 'user',
+      content: message.trim(),
+    });
+  }
 }
